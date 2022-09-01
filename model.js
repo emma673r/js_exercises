23105fed03840b0ab8ac633fad90db7b4849fead
emma673r <90177139+emma673r@users.noreply.github.com>
2022-09-01 12:13:45 +0200

    model update

--- model.js
+++ model.js
@@ -6,7 +6,6 @@ const button = document.querySelector("button");
 
 // controller
 
-
 init();
 function init() {
   button.addEventListener("click", buttonClick);
@@ -14,18 +13,36 @@ function init() {
 }
 
 function addNameToModel(name) {
-    model.push(name);
+  model.push(name);
+}
+
+function removeName(name) {
+  console.log(model.indexOf(name));
+  model.splice(model.indexOf(name), 1);
+  updateView();
 }
 
 function updateView() {
-    h2Names.textContent = model.join(" ");
+  // h2Names.textContent = model.join(" ");
+
+  h2Names.innerHTML = "";
+  model.forEach((each) => {
+    h2Names.innerHTML += `<span class="clickname">${each}</span> `;
+  });
+
+  document.querySelectorAll(".clickname").forEach((span) => {
+    span.addEventListener("click", clickSpan);
+  });
 }
 
 // view
 
 function buttonClick() {
-    addNameToModel(inputName.value);
-    updateView();
+  addNameToModel(inputName.value);
+  updateView();
 }
 
-
+function clickSpan(event) {
+  console.log(`clickSpan`, event.typeof);
+  removeName(event.target.textContent);
+}
